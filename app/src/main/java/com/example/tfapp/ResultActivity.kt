2d7980d90891be45822b0b325c443b3d3dbc32c1
Taek-B <gkdns1004@gmail.com>
2022-11-15 16:17:51 +0900

    카메라 기능 추가

--- app/src/main/java/com/example/tfapp/ResultActivity.kt
+++ app/src/main/java/com/example/tfapp/ResultActivity.kt
@@ -1,20 +1,55 @@
 package com.example.tfapp
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import java.io.IOException
+import java.util.*
 
 class ResultActivity : AppCompatActivity() {
+    lateinit var cls: ClassifierWithModel
     lateinit var morebtn: Button
     lateinit var resultimg: ImageView
     lateinit var resulttxt: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
+        cls = ClassifierWithModel(this)
+        try {
+            cls.init()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
 
+        morebtn = findViewById(R.id.restart_btn)
+        resultimg = findViewById(R.id.result_img)
+        resulttxt = findViewById(R.id.result_txt)
+
+        val byteArray = intent.getByteArrayExtra("image")
+        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+
+        if (bitmap != null) {
+            val (first, second) = cls.classify(bitmap)
+            val resultStr: String =
+                java.lang.String.format(Locale.ENGLISH, "%s", first)
+            resultimg.setImageBitmap(bitmap)
+            resulttxt.setText(resultStr+"\n"+ second*100 + '%')
+
+        }
+
+
+        morebtn.setOnClickListener {
+            val intent = Intent(applicationContext, InputActivity::class.java)
+            finish()
+            cls.finish()
+            startActivity(intent)
+        }
     }
 }
